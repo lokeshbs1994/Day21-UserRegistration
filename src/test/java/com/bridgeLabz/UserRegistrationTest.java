eac4582e696d3c38d22ae9e694f060e555f545c4
lokeshbs1994 <lokeshbs619@gmail.com>
2022-03-07 13:12:59 +0530

    ADDED: Check First name entered by user is vaild or not

---
+++ src/test/java/com/bridgeLabz/UserRegistrationTest.java
@@ -0,0 +1,20 @@
+package com.bridgeLabz;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UserRegistrationTest {
+    UserRegistration userRegistration = new UserRegistration();
+
+    @Test
+    public void givenFirstName_WhenValid_ShouldReturnTrue(){
+        boolean valid = userRegistration.firstName("Loki");
+        Assert.assertEquals(true,valid);
+    }
+
+    @Test
+    public void  givenFirstName_WhenInvalid_ShouldReturnTrue(){
+        boolean valid = userRegistration.firstName("loki");
+        Assert.assertEquals(false,valid);
+    }
+}
